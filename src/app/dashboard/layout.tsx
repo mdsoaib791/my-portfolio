6836c84dd193082c879ec4163aec2b96cd7ba730
Soaib <mdsoaib791@gmail.com>
2025-03-02 11:01:57 +0530

    refactor

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
     <div className="min-h-screen bg-third">
       <div className={`transition-all duration-300 ease-in-out bg-primary min-h-screen fixed  z-50 top-0 left-0 ${!isOpen ? 'w-[250px]' : 'w-[80px]'}`}
       >
-        <Sidenav />
+        {/* <Sidenav /> */}
       </div>
       <div className={`transition-all relative duration-300 ease-in-out ${!isOpen ? 'ms-[250px]' : 'ms-[80px]'}`}>
         <main className="p-3 pt-0">
