b147459c2eae245525b313608f9f9ae1e3c9ad36
Soaib <mdsoaib791@gmail.com>
2025-04-12 15:49:07 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/home/about.tsx
+++ src/components/home/about.tsx
@@ -13,12 +13,12 @@ export default function About() {
             <p className="text-background">I&apos;m passionate about writing reusable components with <strong>Tailwind CSS</strong>, enhancing developer experience through modular code, and continuously learning new technologies to stay sharp in a fast-evolving ecosystem.</p>
             <p className="text-background">Outside of coding, you&apos;ll often find me <strong>unwinding with a game of cricket</strong> or catching a match. Whether I&apos;m playing with friends or watching a close chase, cricket is my go-to for recharging. It&apos;s my way of balancing screen time with some real-time excitement.</p>
             <p className="text-background">My approach to development is simple: <strong>&quot;Build it right, make it last, and keep it user-focused.&quot;</strong></p>
-            <p>It&apos;s a great day to learn React!</p>
-            <p>Don&apos;t forget to check out the documentation.</p>
-            <p>Here&apos;s an example of a component.</p>
-            <p>React&apos;s flexibility is unmatched.</p>
-            <p>Use &quot;props&quot; to pass data to components.</p>
-            <p>React&apos;s &quot;state&quot; is powerful for dynamic UIs.</p>
+            <p className="text-background">It&apos;s a great day to learn React!</p>
+            <p className="text-background">Don&apos;t forget to check out the documentation.</p>
+            <p className="text-background">Here&apos;s an example of a component.</p>
+            <p className="text-background">React&apos;s flexibility is unmatched.</p>
+            <p className="text-background">Use &quot;props&quot; to pass data to components.</p>
+            <p className="text-background">React&apos;s &quot;state&quot; is powerful for dynamic UIs.</p>
           </div>
           <div className="border-2 border-[#f5f5f5] rounded-full rounded-l-none overflow-hidden flex justify-center items-center">
             <Image src={"/images/aidp.jpg"} alt="Hero Image" width={300} height={300} className="w-full rounded-full rounded-l-none" />
